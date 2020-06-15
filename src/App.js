c42872e66c0f39969fed89dcd8bb4e95ccc18e01
Otavio Pimentel <otaviopimentel@Otavios-MacBook-Pro.local>
2020-06-15 17:47:25 -0400

    Testes de AppBar, Drawer e List

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Button } from '@material-ui/core';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Typography from '@material-ui/core/Typography';
+import Drawer from '@material-ui/core/Drawer';
+import { List } from '@material-ui/core';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
 
 function App() {
+
+  const [open, setOpen] = useState(false)
+
   return (
     <div className="App">
+      <AppBar position='fixed'>
+      <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu" onClick={() => setOpen(true)} >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" >
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      <Drawer anchor='left' open={open} onClose={() => setOpen(false)}>
+        <List style={{width: '240px'}}>
+          {['Fardão','Fardinho','Usuário'].map((name, index) => (
+            <ListItem button key={name}>
+            <ListItemIcon> {index > 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={name} />
+          </ListItem>
+          ))}
+        </List>
+      </Drawer>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit src/App.js and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <Button variant="contained" color="secondary">
           Learn React
-        </a>
+        </Button>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+//Import que realiza as mesmas funções do Normalize.js
+import { CssBaseline } from '@material-ui/core';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+
+//gerar tema usando o CreateMuiTheme
+const theme = createMuiTheme({
+  palette: {
+      background: {
+        default: 'rgb(255,255,0)'},
+    primary: {
+      main: 'rgb(0,150,50)',
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <ThemeProvider theme={theme}>
+      <CssBaseline>
+        <App />
+      </CssBaseline>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
